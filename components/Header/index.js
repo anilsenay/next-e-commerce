45ea405828475fae14accd7b507df47de6df8e39
Anıl Şenay <anilsenay3@gmail.com>
2020-10-06 19:35:49 +0300

    Add header items without style

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,7 +1,30 @@
 import React from "react";
+import Link from "next/link";
 
 import styles from "./header.module.scss";
 
 export default function Header() {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <Link href="/">
+        <a className={styles.logo}>Shopping</a>
+      </Link>
+      <input
+        className={styles.searchBar}
+        placeholder="Search for products, brands and more... "
+      />
+      <div className={styles.rightContent}>
+        <div className={styles.cartContainer}>
+          <Link href="/">
+            <span>Cart: 0</span>
+          </Link>
+        </div>
+        <div className={styles.profileContainer}>
+          <Link href="/">
+            <span>Hello Guest</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
--- components/Layout/index.js
+++ components/Layout/index.js
@@ -6,8 +6,10 @@ import Header from "../Header";
 export default function Layout({ children }) {
   return (
     <div className={styles.container}>
-      <Header />
-      {children}
+      <div className={styles.content}>
+        <Header />
+        {children}
+      </div>
     </div>
   );
 }
