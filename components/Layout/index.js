8e438078ce47a4da95898772b89f9231089cc914
Anıl Şenay <anilsenay3@gmail.com>
2020-10-07 14:53:22 +0300

    Update layout after adding categories

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -2,13 +2,17 @@ import React from "react";
 
 import styles from "./layout.module.scss";
 import Header from "../Header";
+import CategoriesBar from "components/Categories";
 
 export default function Layout({ children }) {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
         <Header />
-        {children}
+        <div className={styles.main}>
+          <CategoriesBar />
+          {children}
+        </div>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <h1 className={styles.title}>New In</h1>
+      </main>
     </div>
   );
 }
