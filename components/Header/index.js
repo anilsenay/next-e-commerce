0bd98b55a0e81ae4468dc161a828b18b7c847a5a
Anıl Şenay <anilsenay3@gmail.com>
2020-10-07 13:57:00 +0300

    Add icons to header and restyle

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,28 +3,49 @@ import Link from "next/link";
 
 import styles from "./header.module.scss";
 
+import SearchIcon from "@/icons/search";
+import CartIcon from "@/icons/cart";
+import ArrowIcon from "@/icons/arrow";
+
 export default function Header() {
   return (
-    <div className={styles.container}>
+    <nav className={styles.container}>
       <Link href="/">
         <a className={styles.logo}>Shopping</a>
       </Link>
-      <input
-        className={styles.searchBar}
-        placeholder="Search for products, brands and more... "
-      />
-      <div className={styles.rightContent}>
-        <div className={styles.cartContainer}>
-          <Link href="/">
-            <span>Cart: 0</span>
-          </Link>
-        </div>
-        <div className={styles.profileContainer}>
-          <Link href="/">
-            <span>Hello Guest</span>
-          </Link>
-        </div>
+      <div className={styles.searchContainer}>
+        <SearchIcon
+          width={20}
+          height={20}
+          fill="grey"
+          className={styles.searchIcon}
+        />
+        <input
+          className={styles.searchInput}
+          placeholder="Search for products, brands and more... "
+        />
       </div>
-    </div>
+      <div className={styles.rightContent}>
+        <Link href="/">
+          <div className={styles.cartContainer}>
+            <CartIcon width={20} height={20} className={styles.cartIcon} />
+            <span>Cart: 0</span>
+          </div>
+        </Link>
+
+        <Link href="/">
+          <div className={styles.profileContainer}>
+            <img
+              src="https://picsum.photos/200/200"
+              className={styles.profilePhoto}
+            />
+            <span>
+              Hello <span style={{ fontWeight: "normal" }}>Guest</span>
+            </span>
+            <ArrowIcon width={10} height={10} className={styles.arrowIcon} />
+          </div>
+        </Link>
+      </div>
+    </nav>
   );
 }
