563d8d7f02a7d1c708dcfa85c17d21b405ce139a
Anıl Şenay <anilsenay3@gmail.com>
2020-10-09 01:52:22 +0300

    Create favourite button for vertical card

--- components/HomeCard/vertical-card.js
+++ components/HomeCard/vertical-card.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 import styles from "./vertical.module.scss";
+import HeartIcon from "@/icons/heart";
 
 export default function VerticalCard({
   bgColor,
+  brand,
   name,
   price,
   image,
@@ -14,10 +16,14 @@ export default function VerticalCard({
       className={styles.verticalCard}
       style={{ backgroundColor: bgColor || "" }}
     >
+      <div className={styles.favContainer}>
+        <HeartIcon width={16} height={16} />
+      </div>
       <div className={styles.imageContainer}>
         {image && <img className={styles.image} src={image} />}
       </div>
       <div className={styles.textContainer}>
+        <h4>{brand}</h4>
         <h4>{name}</h4>
         <span className={styles.price}>{price || 0}$</span>
       </div>
