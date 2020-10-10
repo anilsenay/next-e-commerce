ad9b4046b422c0a98f0f9a321f9a2022a4ea64b9
Anıl Şenay <anilsenay3@gmail.com>
2020-10-10 23:18:13 +0300

    Create add address modal

---
+++ pages/account/addresses/add-address.js
@@ -0,0 +1,137 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+import styles from "./add.module.scss";
+import Input from "@/components/Input";
+import Button from "@/components/Button";
+
+const schema = yup.object().shape({
+  title: yup
+    .string()
+    .required("* Title is required.")
+    .min(2, "* Title is too short"),
+  city: yup
+    .string()
+    .required("* City is required.")
+    .min(2, "* City is too short"),
+  region: yup.string().required("* Region is required."),
+  zipcode: yup.string().required("* Zip Code is required."),
+  full_address: yup.string().required("* Address Line is required."),
+});
+
+export default function AddAddress({ closeEvent }) {
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data) => console.log(data);
+
+  const closeModal = (target) => {
+    target?.id === "container" && closeEvent();
+  };
+
+  return (
+    <div
+      className={styles.container}
+      id="container"
+      onClick={(e) => closeModal(e.target)}
+    >
+      <div className={styles.content}>
+        <div className={styles.header}>
+          <h4>Add New Address</h4>
+          <div onClick={closeEvent}>×</div>
+        </div>
+        <hr />
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          style={{ display: "flex", flexDirection: "column", paddingTop: 30 }}
+        >
+          <div className={styles.inputContainer}>
+            <span>Address Title</span>
+            <Input
+              name="title"
+              noMargin
+              register={register}
+              placeholder="Home, Office, etc."
+              error={errors.title}
+            />
+          </div>
+          {errors.title && (
+            <span style={{ color: "red", marginTop: 4, fontSize: 14 }}>
+              {errors.title.message}
+            </span>
+          )}
+
+          <div className={styles.inputContainer}>
+            <span>City</span>
+            <Input
+              name="city"
+              noMargin
+              register={register}
+              placeholder="New York, London, etc."
+              error={errors.city}
+            />
+          </div>
+          {errors.city && (
+            <span style={{ color: "red", marginTop: 4, fontSize: 14 }}>
+              {errors.city.message}
+            </span>
+          )}
+
+          <div className={styles.inputContainer}>
+            <span>Region</span>
+            <Input
+              name="region"
+              noMargin
+              register={register}
+              placeholder="France, Italy, etc."
+              error={errors.region}
+            />
+          </div>
+          {errors.region && (
+            <span style={{ color: "red", marginTop: 4, fontSize: 14 }}>
+              {errors.region.message}
+            </span>
+          )}
+
+          <div className={styles.inputContainer}>
+            <span>Zip Code</span>
+            <Input
+              name="zipcode"
+              noMargin
+              register={register}
+              placeholder=""
+              error={errors.zipcode}
+            />
+          </div>
+          {errors.zipcode && (
+            <span style={{ color: "red", marginTop: 4, fontSize: 14 }}>
+              {errors.zipcode.message}
+            </span>
+          )}
+
+          <div className={styles.inputContainer}>
+            <span>Address Line</span>
+            <Input
+              name="full_address"
+              noMargin
+              register={register}
+              placeholder="123 Main Street, New York, NY 10030, etc."
+              error={errors.full_address}
+            />
+          </div>
+          {errors.full_address && (
+            <span style={{ color: "red", marginTop: 4, fontSize: 14 }}>
+              {errors.full_address.message}
+            </span>
+          )}
+          <Button type="submit" style={{ marginBottom: 0 }}>
+            Add Address
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+}
--- pages/account/addresses/index.js
+++ pages/account/addresses/index.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 import AccountSidebar from "@/components/AccountSidebar";
 import Layout from "@/components/Layout";
 import AddressCard from "@/components/AddressCard";
+import AddAddress from "./add-address";
 
 import styles from "./address.module.scss";
 import { useAuth } from "@/firebase/context";
 
 export default function Addresses() {
+  const [toggleModal, setModal] = useState(false);
+
   const { user } = useAuth();
+
+  console.log("toogle: " + toggleModal);
 
   return (
     <Layout noCategories>
@@ -19,13 +24,19 @@ export default function Addresses() {
           {user?.orders?.length === 0 ? (
             <div style={{ display: "flex", flexDirection: "column" }}>
               <p>You have not any address</p>
-              <button className={styles.addAddress}>
+              <button
+                className={styles.addAddress}
+                onClick={() => setModal(true)}
+              >
                 <p>+</p>Add New Address
               </button>
             </div>
           ) : (
             <div className={styles.addresses}>
-              <button className={styles.addAddress}>
+              <button
+                className={styles.addAddress}
+                onClick={() => setModal(true)}
+              >
                 <p>+</p>Add New Address
               </button>
               <AddressCard />
@@ -35,6 +46,7 @@ export default function Addresses() {
             </div>
           )}
         </div>
+        {toggleModal && <AddAddress closeEvent={() => setModal(false)} />}
       </main>
     </Layout>
   );
